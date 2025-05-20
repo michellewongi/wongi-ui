89bbdb37d5963a1954d36688122ad231b22fdcb7
Michelle <michelle.wong.dev@gmail.com>
2025-05-20 10:51:32 -0400

    Set up initial project structure and configured dark mode for Tailwind

---
+++ .storybook/preview.ts
@@ -0,0 +1,36 @@
+import type { Preview } from "@storybook/react";
+import "../src/tailwind.css";
+import {
+  withThemeByClassName,
+  withThemeByDataAttribute,
+} from "@storybook/addon-themes";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: "light",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+    }),
+    withThemeByDataAttribute({
+      themes: {
+        light: "light",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+      attributeName: "data-mode",
+    }),
+  ],
+};
+
+export default preview;
